df871c34fa68675ba9542ee1d4b4010a1d50a0bd
johann0301 <johann.bezerra27@gmail.com>
2025-11-04 22:32:40 -0300

    Creation of SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,8 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.dao.impl.SellerDaoJDBC;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,10 +12,13 @@ public class Program {
     public static void main(String[] args) {
 
         Department dep =  new Department(1, "Software Engineering");
-        System.out.println(dep);
 
         Seller seller =  new Seller(2, "Johann", "johann.bezerra27@gmail.com", new Date(), 10000.00, dep);
 
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        System.out.println(dep);
         System.out.println(seller);
+        System.out.println(sellerDao);
     }
 }
